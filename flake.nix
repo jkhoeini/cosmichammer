50552718aaa4f3afae37a04ec8492c13adb9b674
Jonathan Khoeini <mskco.tp@gmail.com>
2026-05-30 21:31:23 +0200

    chore: release v0.5.2

--- flake.nix
+++ flake.nix
@@ -9,10 +9,10 @@
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
 
-      version = "0.5.1";
+      version = "0.5.2";
       src = pkgs.fetchurl {
         url = "https://github.com/jkhoeini/cosmichammer/releases/download/v${version}/Cosmic-Hammer-${version}.dmg";
-        hash = "sha256-XmJsri8swymx5BUWg2MBM9im5AytetuUOhLTsaEI44s=";
+        hash = "sha256-d+f7r/LiemexGdOmTy6JHe4nUpyCPI+nYWleHCzdoo0=";
       };
 
       cosmic-hammer = pkgs.stdenvNoCC.mkDerivation {
