c36280d8b5d6140fa1ab3c19c0a8126a96a1bdd0
Jonathan Khoeini <mskco.tp@gmail.com>
2026-06-20 19:21:12 +0200

    chore: update flake.nix to v0.9.1

--- flake.nix
+++ flake.nix
@@ -9,10 +9,10 @@
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
 
-      version = "0.9.0";
+      version = "0.9.1";
       src = pkgs.fetchurl {
         url = "https://github.com/jkhoeini/cosmichammer/releases/download/v${version}/Cosmic-Hammer-${version}.dmg";
-        hash = "sha256-m/e5kKMBkQ6em2rixl7nZtI6DtRru/hNrixjsjfqQis=";
+        hash = "sha256-wmbnsSDMKJ/Qo31DJv3dQlhbDCh1FlTFs1Mjwt/JG7Y=";
       };
 
       cosmic-hammer = pkgs.stdenvNoCC.mkDerivation {
